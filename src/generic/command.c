a62adaef206229838b7053266a50ee43a7679e1f
David Welton <davidw@apache.org>
2002-02-27 08:45:59 +0000

    Whitespace change.

--- src/generic/command.c
+++ src/generic/command.c
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------------
  * command.c -- implement web::command for websh3
  * nca-073-9
- * 
+ *
  * Copyright (c) 1996-2000 by Netcetera AG.
  * Copyright (c) 2001 by Apache Software Foundation.
  * All rights reserved.
